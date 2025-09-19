3075f98ecd48bc88ca9217c1d65a5e5bed130f22
Faridpin <farid.mahmudlu.std@bhos.edu.az>
2025-09-19 08:56:50 +0000

    1

--- web_application_security/0x01_owasp_top_10/1-xor_decoder.sh
+++ web_application_security/0x01_owasp_top_10/1-xor_decoder.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
-printf '%s' "$({ echo -n "${1#\{xor\}}" | base64 -d | xxd -p -c1 | awk '{printf "\\x%02x", strtonum("0x"$1) ^ 0x5F}' ; })"
+printf '%b' "$(echo -n "${1#\{xor\}}" \
+    | base64 -d \
+    | xxd -p -c1 \
+    | awk '{printf "\\x%02x", strtonum("0x"$1) ^ 0x5F}')"
