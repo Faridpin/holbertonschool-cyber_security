b829428101ba122206d6c6cfd1fa45cddcda1e90
root <root@kali.kali>
2025-11-01 18:14:15 +0400

    1

--- linux_security/0x03_protocols_servers/1-audit.sh
+++ linux_security/0x03_protocols_servers/1-audit.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-cat /etc/ssh/sshd_config
+grep -E 'Include|KbdInteractiveAuthentication|UsePAM|X11Forwarding|PrintMotd|AcceptEnv|Subsystem|PasswordAuthentication|PermitRootLogin|AuthorizedKeysFile|TCPKeepAlive' /etc/ssh/sshd_config /etc/ssh/sshd_config.d/*.conf 2>/dev/null
+
